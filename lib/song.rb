452fd3474d4a943306805edc4dfeee6fedc90b2c
John Tuttle <thejwt@gmail.com>
2017-09-28 20:36:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,11 @@ class Song
 
   def self.new_from_filename(name)
     temp_name = name.scan(/^[a-zA-Z0-9_\s-]+/) #slightly modded version of regex formula Paul Fox's formula: http://regexlib.com/REDetails.aspx?regexp_id=507
-    temp_name.split(" - ")
+    name_artist_array = temp_name.split(" - ")
+    new_song = self.create
+    new_song.name = name_artist_array[1]
+    new_song.artist_name = name_artist_array[0]
+    
   end
 
   def self.all
