80130d09f8c9dd32421c0e7c34d5883b5ffe83e1
John Tuttle <thejwt@gmail.com>
2017-09-28 20:06:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,6 +31,14 @@ class Song
     end
   end
 
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) != nil
+      return self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
   def self.all
     @@all
   end
