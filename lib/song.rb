ea4782ce887e051f864cb5a17dcc0f6791d0b11e
John Tuttle <thejwt@gmail.com>
2017-09-28 20:02:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,14 @@ class Song
     create_song
   end
 
+  def self.create_by_name(name)
+    create_song = self.new
+    create_song.name = name
+    @@all << create_song
+    create_song.save
+    create_song
+  end
+
   def self.all
     @@all
   end
